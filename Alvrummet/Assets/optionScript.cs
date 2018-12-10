a38589ca9cc1879c25f96de2efd1a3b374cace4a
Hannes Gustafsson <>
2018-12-10 10:50:13 +0100

    q

--- Alvrummet/Assets/optionScript.cs
+++ Alvrummet/Assets/optionScript.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class optionScript : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    GameObject menu;
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        menu = GameObject.Find("MenuSystemimage");
+        menu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                string clicked = EventSystem.current.currentSelectedGameObject.name;
+
+                if (clicked.Equals("OptionButton"))
+                {
+                    menu.SetActive(true);
+                }
+
+                if(clicked.Equals("SwedishButton"))
+                {
+                    Question.getInstance().setLang("Swe");
+                }
+
+                if (clicked.Equals("EnglishButton"))
+                {
+                    Question.getInstance().setLang("Eng");
+                }
+
+
+
+            }
+        }
+    }
 }
