5cea594b0f871632024725dfb1fbfe925e1520dd
Hannes Gustafsson <>
2018-12-11 10:19:10 +0100

    q

--- Alvrummet/Assets/GameMode.cs
+++ Alvrummet/Assets/GameMode.cs
@@ -80,7 +80,7 @@ public class GameMode
     public void showQuestion(){
         questionImg.SetActive(true);
         questionImg.transform.GetChild(0).GetComponent<Text>().text = sn.GetQ();
-        questionImg.transform.GetChild(2).GetComponent<Image>().sprite = Resources.Load<Sprite>("masthugget");
+        questionImg.transform.GetChild(2).GetComponent<Image>().sprite = Resources.Load<Sprite>(sn.getImg());
     }
 
     public void getNextQuestion(){
